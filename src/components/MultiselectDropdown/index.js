235b8a5889ca7982899eb5911794b079c883d9a9
Conroy Whitney <conroy.whitney@gmail.com>
2020-07-23 12:34:25 -0800

    Remove console.logs & simplify `overrideStrings`

--- src/components/MultiselectDropdown/index.js
+++ src/components/MultiselectDropdown/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import MultiSelect from "@khanacademy/react-multi-select";
-import { compose, isNil, map, path, reject, uniq } from "ramda";
+import { compose, isNil, map, path, pick, reject, uniq } from "ramda";
 
 const filterOptions = (options, filter) => {
-  console.log("filterOptions", "options", options, "filter", filter);
-
   const optionIncludesText = (option) =>
     (option.label || "").toLowerCase().includes(filter);
 
@@ -36,8 +34,6 @@ class MultiselectDropdown extends Component {
     const { itemLabel, itemList, itemValue } = this.props;
     const record = path(["_meta", "record"], item);
 
-    console.log("generateOption", "item", item, "record", record);
-
     if (!record) return null;
 
     return {
@@ -49,46 +45,24 @@ class MultiselectDropdown extends Component {
   generateOptions() {
     const { itemLabel, itemList, itemValue } = this.props;
 
-    console.log(
-      "generateOptions",
-      "itemLabel",
-      itemLabel,
-      "itemList",
-      itemList,
-      "itemValue",
-      itemValue
-    );
-
     if (!itemLabel || !itemList || !itemValue) return [];
 
     return compose(reject(isNil), uniq, map(this.generateOption))(itemList);
   }
 
   render() {
-    console.log("render", "props", this.props);
-
-    const {
-      _height: height,
-      _width: width,
-      allItemsAreSelectedLabel,
-      searchLabel,
-      selectAllLabel,
-      selectSomeItemsLabel,
-    } = this.props;
+    const { _height: height, _width: width } = this.props;
     const { selected } = this.state;
-
-    console.log("render", "selected", selected);
+    const overrideStrings = pick(
+      ["allItemsAreSelected", "search", "selectAll", "selectSomeItems"],
+      this.props
+    );
 
     return (
       <MultiSelect
         filterOptions={filterOptions}
         options={this.generateOptions()}
-        overrideStrings={{
-          selectSomeItems: selectSomeItemsLabel,
-          allItemsAreSelected: allItemsAreSelectedLabel,
-          selectAll: selectAllLabel,
-          search: searchLabel,
-        }}
+        overrideStrings={overrideStrings}
         selected={selected}
         onSelectedChanged={(selected) => this.setState({ selected })}
         styles={{ height, width }}
