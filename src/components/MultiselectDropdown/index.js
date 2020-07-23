f197ec0d5c42801489293313ed5017f5338c8d98
Conroy Whitney <conroy.whitney@gmail.com>
2020-07-23 12:29:16 -0800

    Pass in list of objects to render dropdown

--- src/components/MultiselectDropdown/index.js
+++ src/components/MultiselectDropdown/index.js
@@ -1,26 +1,72 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import MultiSelect from "@khanacademy/react-multi-select";
-
-const options = [
-  { label: "One", value: 1 },
-  { label: "Two", value: 2 },
-  { label: "Three", value: 3 },
-];
+import { compose, isNil, map, path, reject, uniq } from "ramda";
 
 const filterOptions = (options, filter) => {
+  console.log("filterOptions", "options", options, "filter", filter);
+
   const optionIncludesText = (option) =>
     (option.label || "").toLowerCase().includes(filter);
 
   return options.filter(optionIncludesText);
 };
 
+const itemRecord = (item) => {
+  const { _meta: meta } = item;
+
+  if (!meta) return null;
+
+  const { record } = meta;
+};
+
 class MultiselectDropdown extends Component {
-  state = {
-    selected: [],
-  };
+  constructor(props) {
+    super(props);
+
+    this.generateOption = this.generateOption.bind(this);
+    this.generateOptions = this.generateOptions.bind(this);
+
+    this.state = {
+      selected: [],
+    };
+  }
+
+  generateOption(item) {
+    const { itemLabel, itemList, itemValue } = this.props;
+    const record = path(["_meta", "record"], item);
+
+    console.log("generateOption", "item", item, "record", record);
+
+    if (!record) return null;
+
+    return {
+      label: record[itemLabel],
+      value: record[itemValue],
+    };
+  }
+
+  generateOptions() {
+    const { itemLabel, itemList, itemValue } = this.props;
+
+    console.log(
+      "generateOptions",
+      "itemLabel",
+      itemLabel,
+      "itemList",
+      itemList,
+      "itemValue",
+      itemValue
+    );
+
+    if (!itemLabel || !itemList || !itemValue) return [];
+
+    return compose(reject(isNil), uniq, map(this.generateOption))(itemList);
+  }
 
   render() {
+    console.log("render", "props", this.props);
+
     const {
       _height: height,
       _width: width,
@@ -31,10 +77,12 @@ class MultiselectDropdown extends Component {
     } = this.props;
     const { selected } = this.state;
 
+    console.log("render", "selected", selected);
+
     return (
       <MultiSelect
         filterOptions={filterOptions}
-        options={options}
+        options={this.generateOptions()}
         overrideStrings={{
           selectSomeItems: selectSomeItemsLabel,
           allItemsAreSelected: allItemsAreSelectedLabel,
