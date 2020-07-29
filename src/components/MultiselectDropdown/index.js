2aef45b22fa74cbb5b0bb97eff9113d205d5dce3
Conroy Whitney <conroy.whitney@gmail.com>
2020-07-28 23:30:19 -0800

    Initial mobile version with `react-native-multiple-select`

--- src/components/MultiselectDropdown/index.js
+++ src/components/MultiselectDropdown/index.js
@@ -1,17 +1,85 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import MultiSelect from 'react-native-multiple-select';
+
+const items = [{
+  id: '92iijs7yta',
+  name: 'Ondo'
+}, {
+  id: 'a0s0a8ssbsd',
+  name: 'Ogun'
+}, {
+  id: '16hbajsabsd',
+  name: 'Calabar'
+}, {
+  id: 'nahs75a5sg',
+  name: 'Lagos'
+}, {
+  id: '667atsas',
+  name: 'Maiduguri'
+}, {
+  id: 'hsyasajs',
+  name: 'Anambra'
+}, {
+  id: 'djsjudksjd',
+  name: 'Benue'
+}, {
+  id: 'sdhyaysdj',
+  name: 'Kaduna'
+}, {
+  id: 'suudydjsjd',
+  name: 'Abuja'
+  }
+];
 
 class MultiselectDropdown extends Component {
   constructor(props) {
     super(props);
+
+    this.onSelectedItemsChange = this.onSelectedItemsChange.bind(this);
+
+    this.multiSelect = null;
+
+    this.state = {
+      selectedItems : []
+    }
+  }
+
+  onSelectedItemsChange(selectedItems) {
+    this.setState({ selectedItems });
   }
 
   render() {
     const { _height: height, _width: width, stringOverrides } = this.props;
+    const { selectedItems } = this.state;
 
     return (
-      <View>
-        <Text>Mobile MultiselectDropdown</Text>
+      <View style={{ flex: 1 }}>
+        <MultiSelect
+          hideTags
+          items={items}
+          uniqueKey="id"
+          ref={(component) => { this.multiSelect = component }}
+          onSelectedItemsChange={this.onSelectedItemsChange}
+          selectedItems={selectedItems}
+          selectText="Pick Items"
+          searchInputPlaceholderText="Search Items..."
+          onChangeInput={ (text)=> console.log(text)}
+          altFontFamily="ProximaNova-Light"
+          tagRemoveIconColor="#CCC"
+          tagBorderColor="#CCC"
+          tagTextColor="#CCC"
+          selectedItemTextColor="#CCC"
+          selectedItemIconColor="#CCC"
+          itemTextColor="#000"
+          displayKey="name"
+          searchInputStyle={{ color: '#CCC' }}
+          submitButtonColor="#CCC"
+          submitButtonText="Submit"
+        />
+        <View>
+          {this.multiSelect && this.multiSelect.getSelectedItemsExt(selectedItems)}
+        </View>
       </View>
     );
   }
